e345674940732555e53bbb55c8ef32b312c434ad
Marek <Marek@LAPTOP-OIN8AEV8.local>
2018-03-17 19:59:21 +0100

    Resized app window

--- src/code/Main.java
+++ src/code/Main.java
@@ -46,7 +46,7 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 642, 523);
+		frame.setBounds(100, 100, 642, 338);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
